15be39cf32c5029b0560ae62f4d10da55f880488
Oliver Veits <oliver.veits@gmail.com>
2018-10-16 22:18:21 +0200

    epel-release needed for ansible installation for resolving dependencies

--- install-openshift.sh
+++ install-openshift.sh
@@ -85,7 +85,8 @@ yum remove -y ansible
 
 # install the packages for Ansible
 curl -O http://cbs.centos.org/kojifiles/packages/ansible/2.6.5/1.el7/noarch/ansible-2.6.5-1.el7.noarch.rpm
-sudo yum install -y ansible-2.6.5-1.el7.noarch.rpm
+# note: epel is needed to install dependencies like python-keyczar
+yum -y --enablerepo=epel install ansible-2.6.5-1.el7.noarch.rpm
 yum -y --enablerepo=epel install pyOpenSSL
 
 [ ! -d openshift-ansible ] && git clone https://github.com/openshift/openshift-ansible.git
